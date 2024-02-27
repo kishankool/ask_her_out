1bd85fc2ea67e71886d9fe110ea7663e3a406f2f
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2024-02-28 00:57:36 +0530

    added css

--- src/app/chain/Game.js
+++ src/app/chain/Game.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Shark from "../../assets/sharkgif.png";
+import styles from './game.module.css' 
 
 export default function Game() {
-  return <div className="parent-div">{/* Your content here */}</div>;
+  return (
+    <div className={styles.parentDiv}>
+        <p className="text">
+        Want to play a  <span className="special-text">Valentines</span> Game?
+        </p>
+      <img src={Shark} alt="Shark" />
+    </div>
+  );
 }
